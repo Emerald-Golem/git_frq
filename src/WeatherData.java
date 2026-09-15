e05011c7e4b2741fa4853db42ba7173991b8d544
Emerald-Golem <128jlin@frhsd.com>
2026-09-15 13:58:33 -0400

    test longestheatwave

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -36,13 +36,20 @@ public class WeatherData {
 
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
-        weatherData.addTemperature(90);
-        weatherData.addTemperature(80);
-        weatherData.addTemperature(70);
-        weatherData.addTemperature(60);
-        weatherData.addTemperature(51);
-        weatherData.cleanData(70, 80);
-        System.out.println(weatherData.longestHeatWave(70));
+        weatherData.addTemperature(100.5);
+        weatherData.addTemperature(98.5);
+        weatherData.addTemperature(102);
+        weatherData.addTemperature(103.9);
+        weatherData.addTemperature(87.5);
+        weatherData.addTemperature(105.2);
+        weatherData.addTemperature(90.3);
+        weatherData.addTemperature(94.8);
+        weatherData.addTemperature(109.1);
+        weatherData.addTemperature(102.1);
+        weatherData.addTemperature(107.4);
+        weatherData.addTemperature(93.2);
+        System.out.println(weatherData.longestHeatWave(100.5));
+        System.out.println(weatherData.longestHeatWave(95.2));
 
     }
 }
