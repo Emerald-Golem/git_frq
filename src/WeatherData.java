e7e085ac02b5afaa045815d0597fbd1ebdb9bf2a
TropicalEraser <128eyang@frhsd.com>
2026-09-15 13:30:06 -0400

    create LongestHeatWave method

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,4 +1,19 @@
 import java.util.ArrayList;
 public class WeatherData {
     private ArrayList<Double> temperatures;
+
+    public int longestHeatWave (double threshold) {
+        int longest = 0;
+        for (int i = 0; i < temperatures.size(); i++) {
+            int current = 0;
+            while (temperatures.get(i) >= threshold && i < temperatures.size()) {
+                current++;
+                i++;
+            }
+            if (current > longest) {
+                longest = current;
+            }
+        }
+        return longest;
+    }
 }
