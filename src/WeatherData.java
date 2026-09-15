c722b7189c57af7986241dd4b1875cd6e2d325f1
Emerald-Golem <128jlin@frhsd.com>
2026-09-15 13:27:50 -0400

    clean data method

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,4 +1,13 @@
 import java.util.ArrayList;
 public class WeatherData {
     private ArrayList<Double> temperatures;
+
+    public void cleanData(double lower, double upper){
+        for(int i = 0; i < temperatures.size(); i++){
+            if(temperatures.get(i) < lower || temperatures.get(i) > upper){
+                temperatures.remove(i);
+                i--;
+            }
+        }
+    }
 }
